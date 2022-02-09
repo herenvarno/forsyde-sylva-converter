912e7ac10d217adac6933fa7edebc57d80e573b6
Edwared Yang <herenvarno@live.com>
2022-02-09 17:42:36 +0100

    update

--- src/ForsydeSylvaConverter.py
+++ src/ForsydeSylvaConverter.py
@@ -1,9 +1,9 @@
 import sys
-import os
 import xml.etree.ElementTree as et
+from matplotlib.font_manager import json_dump
 import networkx as nx
 import matplotlib.pyplot as plt
-import math
+import json
 
 
 class SDF:
@@ -26,12 +26,13 @@ class FileIO:
         sdf = {"nodes": [], "edges": []}
         for node in root.findall("./nodes"):
             flag_is_process_node = False
+            trait = ""
             for trait in node.findall("./traits"):
-                print(trait.text)
                 if (
                     trait.text == "moc::sdf::SDFComb"
                     or trait.text == "moc::sdf::SDFChannel"
                 ):
+                    trait = trait.text
                     flag_is_process_node = True
                     break
 
@@ -40,6 +41,37 @@ class FileIO:
 
             n = {}
             n["name"] = node.attrib["identifier"]
+            n["type"] = trait
+            
+            if(n["type"] ==  "moc::sdf::SDFComb"):
+                trait = ""
+                for trait in node.findall("./traits"):
+                    if trait.text.startswith("func"):
+                        trait = trait.text
+                        break
+                n["func"] = trait
+
+                prop_map = {}
+                prop_name = []
+                prop_value = []
+                for pn in node.findall("./propertiesNames"):
+                    prop_name.append(pn.text)
+                for pv in node.findall("./propertiesValues"):
+                    prop_value_map = {}
+                    values = []
+                    indices = []
+                    for v in pv.findall("./values"):
+                        values.append(v.attrib["intValue"])
+                    for i in pv.findall("./indexes"):
+                        indices.append(i.text)
+                    for i in range(len(indices)):
+                        prop_value_map[indices[i]] = values[i]
+                    prop_value.append(prop_value_map)
+                for i in range(len(prop_name)):
+                    prop_map[prop_name[i]] = prop_value[i]
+                n["prop"] = prop_map
+                    
+
             n["ports"] = {}
             for port in node.findall("./ports"):
                 p = {}
@@ -50,7 +82,6 @@ class FileIO:
         for edge in root.findall("./edges"):
             flag_is_data_edge = False
             for trait in edge.findall("./traits"):
-                print(trait.text)
                 if trait.text == "moc::sdf::SDFDataEdge":
                     flag_is_data_edge = True
                     break
@@ -71,11 +102,11 @@ class FileIO:
                             )
                             exit(-1)
                         if n["ports"][e["src_port"]]["io"] == "":
-                            n["ports"][e["src_port"]]["io"] = "in"
+                            n["ports"][e["src_port"]]["io"] = "out"
                         elif n["ports"][e["src_port"]]["io"] == "in":
-                            pass
-                        elif n["ports"][e["src_port"]]["io"] == "out":
                             n["ports"][e["src_port"]]["io"] = "in_out"
+                        elif n["ports"][e["src_port"]]["io"] == "out":
+                            pass
                         elif n["ports"][e["src_port"]]["io"] == "in_out":
                             pass
                         else:
@@ -94,11 +125,11 @@ class FileIO:
                             )
                             exit(-1)
                         if n["ports"][e["dst_port"]]["io"] == "":
-                            n["ports"][e["dst_port"]]["io"] = "out"
+                            n["ports"][e["dst_port"]]["io"] = "in"
                         elif n["ports"][e["dst_port"]]["io"] == "in":
-                            n["ports"][e["dst_port"]]["io"] = "in_out"
+                           pass
                         elif n["ports"][e["dst_port"]]["io"] == "out":
-                            pass
+                             n["ports"][e["dst_port"]]["io"] = "in_out"
                         elif n["ports"][e["dst_port"]]["io"] == "in_out":
                             pass
                         else:
@@ -110,7 +141,7 @@ class FileIO:
         return sdf
 
     def save(self, filename, sylva_sdf):
-        pass
+        
 
 
 class Visulizer:
@@ -119,7 +150,7 @@ class Visulizer:
     def __init__(self):
         pass
 
-    def visualize(self, sdf):
+    def visualize_forsyde(self, sdf):
         g = nx.MultiDiGraph()
         for n in sdf["nodes"]:
             g.add_node(n["name"])
@@ -132,9 +163,25 @@ class Visulizer:
             "node_shape": "s",
             "node_color": "#aaaaaa",
         }
-        k = 5 / math.sqrt(g.order())
         subax1 = plt.subplot(111)
-        nx.draw(g, pos=nx.spring_layout(g, k), **options)
+        nx.draw(g, pos=nx.spring_layout(g), **options)
+        plt.show()
+    
+    def visualize_sylva(self, sdf):
+        g = nx.MultiDiGraph()
+        for n in sdf["actors"]:
+            g.add_node(n["index"])
+        for e in sdf["edges"]:
+            g.add_edge(e["src_actor"], e["dest_actor"])
+
+        options = {
+            "with_labels": True,
+            "node_size": 3000,
+            "node_shape": "s",
+            "node_color": "#aaaaaa",
+        }
+        subax1 = plt.subplot(111)
+        nx.draw(g, pos=nx.spring_layout(g), **options)
         plt.show()
 
 
@@ -145,7 +192,99 @@ class ForsydeSylvaConverter:
         pass
 
     def convert(self, forsyde_sdf):
-        pass
+
+        # Remove the SDFChannel nodes and reconnect the edges
+        add_edges = []
+        for node in forsyde_sdf["nodes"]:
+            if node["type"] == "moc::sdf::SDFChannel":
+                for e0 in forsyde_sdf["edges"]:
+                    if e0["src"] == node["name"] and e0["src_port"] == "consumer":
+                        for e1 in forsyde_sdf["edges"]:
+                            if e1["dst"] == node["name"] and e1["dst_port"] == "producer":
+                                e = {"src": e1["src"], "src_port": e1["src_port"], "dst": e0["dst"], "dst_port": e0["dst_port"]}
+                                add_edges.append(e)
+
+        for e in add_edges:
+            forsyde_sdf["edges"].append(e)
+
+        for node in forsyde_sdf["nodes"]:
+            if node["type"] == "moc::sdf::SDFChannel":
+                flag_changed=True
+                while(flag_changed):
+                    flag_changed = False
+                    for e0 in forsyde_sdf["edges"]:
+                        if e0["src"] == node["name"]:
+                            forsyde_sdf["edges"].remove(e0)
+                            flag_changed = True
+                            break
+                    for e0 in forsyde_sdf["edges"]:
+                        if e0["dst"] == node["name"]:
+                            forsyde_sdf["edges"].remove(e0)
+                            flag_changed = True
+                            break
+
+        sylva_sdf = {"actors":[], "edges":[]}
+
+        for e in forsyde_sdf["edges"]:
+            sylva_sdf ["edges"].append({"src_actor": e["src"], "src_port": e["src_port"], "dest_actor": e["dst"], "dest_port": e["dst_port"]})
+
+        name_to_id = {}
+        id_to_name = {}
+        i=0
+        for node in forsyde_sdf["nodes"]:
+            if node["type"] == "moc::sdf::SDFComb":
+                name_to_id[node["name"]] = i
+                id_to_name[i] = node["name"]
+                n = {}
+                n["index"] = i
+                n["name"] = node["func"]
+                sylva_sdf["actors"].append(n)
+                i=i+1
+
+                j=0
+                k=0
+                output_ports = []
+                input_ports = []
+                name_to_port ={}
+                for port_name, port in node["ports"].items():
+                    p = {}
+                    p["name"] = port["name"]
+                    p["index"] = j
+                    if port["io"] != "in" and port["io"] != "out":
+                        continue
+                    if port["io"] == "in":
+                        p["count"] = int(node["prop"]["consumption"][p["name"]])
+                        input_ports.append(p)
+                        name_to_port[p["name"]] = j
+                        j=j+1
+                    elif port["io"] == "out":
+                        p["count"] = int(node["prop"]["production"][p["name"]])
+                        output_ports.append(p)
+                        name_to_port[p["name"]] = k
+                        k=k+1
+                n["input_ports"] = input_ports
+                n["output_ports"] = output_ports
+                
+                for e in sylva_sdf["edges"]:
+                    if e["src_actor"] == node["name"]:
+                        e["src_actor"] = n["index"]
+                        e["src_port"] = name_to_port[e["src_port"]]
+                        
+                    if e["dest_actor"] == node["name"]:
+                        e["dest_actor"] = n["index"]
+                        e["dest_port"] = name_to_port[e["dest_port"]]
+
+        for node in sylva_sdf["actors"]:
+            node ["prev"] = []
+            node ["next"] = []
+            for e in sylva_sdf["edges"]:
+                if e["src_actor"] == node["index"]:
+                    node["next"].append(e)
+                if e["dest_actor"] == node["index"]:
+                    node["prev"].append(e)
+
+        return sylva_sdf
+
 
 
 if __name__ == "__main__":
@@ -159,5 +298,9 @@ if __name__ == "__main__":
     sdf = io.load(forsyde_xml)
     print(sdf)
 
+    conv = ForsydeSylvaConverter()
+    sdf_sylva = conv.convert(sdf)
+    print(sdf_sylva)
+
     vs = Visulizer()
-    vs.visualize(sdf)
+    vs.visualize_sylva(sdf_sylva)
